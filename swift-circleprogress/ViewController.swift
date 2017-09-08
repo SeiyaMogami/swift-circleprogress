a856f499a3e433b05fa229cc00bfdb2eac24ef19
最上 聖也 <mogami_s@mti.co.jp>
2017-09-08 11:14:00 +0900

    Initial Commit

---
+++ swift-circleprogress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-circleprogress
+//
+//  Created by 最上 聖也 on 2017/09/08.
+//  Copyright © 2017年 Seiya Mogami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
